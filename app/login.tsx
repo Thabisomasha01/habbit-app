c7fb85173e2d35b875739ae922eea61f5911e332
Thabisomasha01 <mashathabiso2006@gmail.com>
2025-11-12 20:27:38 +0000

    initial commit

---
+++ app/login.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native'
+
+export default function LoginScreen() {
+    return (
+        <View>
+            {""}
+            <Text>Hello this is  a LoginScreen</Text>
+        </View>
+    )
+}
